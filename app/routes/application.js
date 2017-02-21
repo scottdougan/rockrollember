a46bcfd5fd5e6d6276bd0fff41ff8283f46a9e76
Scott Dougan <scottdougan@me.com>
2017-02-20 20:31:08 -0500

    Completed chapter 5: Nested routes

---
+++ app/routes/application.js
@@ -0,0 +1,5 @@
+// app/routes/application.js
+import Ember from 'ember';
+
+export default Ember.Route.extend({ 
+});
---
+++ app/routes/bands/band.js
@@ -0,0 +1,9 @@
+// app/routes/bands/band.js
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model: function(params) {
+    var bands = this.modelFor('bands');
+    return bands.findBy('slug', params.slug); // params.slug is now 'pearl-jam'
+  }
+});
