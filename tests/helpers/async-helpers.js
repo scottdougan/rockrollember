aa2ec89ebec658b5787d2c6d75d5915c2d81fbeb
Scott Dougan <scottdougan@me.com>
2017-02-22 21:17:09 -0500

    Completed chapter 11: Testing

---
+++ tests/helpers/async-helpers.js
@@ -0,0 +1,16 @@
+// tests/helpers/async-helpers.js
+import Ember from 'ember';
+
+function selectBand(app, name) {
+  visit('/')
+  .click('.band-link:contains("' + name + '")');
+
+  return app.testHelpers.wait();
+}
+
+function submit(app, selector) {
+  return triggerEvent(selector, 'submit');
+}
+
+Ember.Test.registerAsyncHelper('selectBand', selectBand);
+Ember.Test.registerAsyncHelper('submit', submit);
